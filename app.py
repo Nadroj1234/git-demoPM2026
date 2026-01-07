087fe8d558e875d1966d68205c0b9c45909e3118
Nadroj1234 <jordank06759@gmail.com>
2026-01-07 12:53:36 -0500

    Refactor print statements in app.py
    Replaced multiple print statements with fewer, more relevant messages.

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 print("Git/Github Demo")
-print("hello from the local repo 0 2")
-
-print("hello from the remote repo 3")
-print("hey local, you aint tuff")
-
-print("hello from the remote repo 1")
-print("hey remote, you a doofus")
+print("this is an okay day - local")
+print("this is a great day")
+print("THis is works for me - local")
