b7d106b08187b84c4c81ee5e8cb1f108e186673f
Jordan Klemm <jordank06759@gmail.com>
2026-01-07 12:56:03 -0500

    merge

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print("Git/Github Demo")
-print("this is an okay day - remote")
+print("this is an okay day - local")
 print("this is a great day")
-print("THis is works for me - remote")
+print("THis is works for me - local")
